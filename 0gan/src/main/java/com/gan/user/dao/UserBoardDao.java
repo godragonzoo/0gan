59f75adefb1e68b3d5d8e4eb1432234c3efc0d97
dmdjdj <dtdad@naver.com>
2021-04-01 10:21:01 +0900

    -

--- 0gan/src/main/java/com/gan/user/dao/UserBoardDao.java
+++ 0gan/src/main/java/com/gan/user/dao/UserBoardDao.java
@@ -8,23 +8,58 @@ import org.springframework.stereotype.Repository;
 import com.gan.user.db.DBManager;
 import com.gan.vo.FaqVo;
 import com.gan.vo.NotiVo;
+import com.gan.vo.ThemePlaceVo;
+import com.gan.vo.ThemeVo;
 
 @Repository
 public class UserBoardDao {
 	
+	/**
+	 * 공지사항 목록 by 박권익
+	 * @param map
+	 * @return
+	 */
 	public List<NotiVo> selectNoti(Map<String, Object> map){
 		return DBManager.selectNoti(map);
 	}
 	
+	/**
+	 * 공지사항 페이징 처리 by 박권익
+	 * @param keyword
+	 * @return
+	 */
 	public int countNoti(String keyword) {
 		return DBManager.countNoti(keyword);
 	}
 
+	/**
+	 * 도움말 페이징 처리 by 박권익
+	 * @param keyword
+	 * @param category
+	 * @return
+	 */
 	public int countFaq(String keyword, String category) {
 		return DBManager.countFaq(keyword, category);
 	}
 
+	/**
+	 * 도움말 목록 by 박권익
+	 * @param map
+	 * @return
+	 */
 	public List<FaqVo> selectFaq(Map<String, Object> map) {
 		return DBManager.selectFaq(map);
+	}
+	
+	/**
+	 * 기획전 목록 by 박권익
+	 * @return
+	 */
+	public List<ThemeVo> selectAllTheme(){
+		return DBManager.selectAllTheme();
+	}
+	
+	public List<ThemePlaceVo> selectThemePlace(int theme_num){
+		return DBManager.selectThemePlace(theme_num);
 	}
 }
