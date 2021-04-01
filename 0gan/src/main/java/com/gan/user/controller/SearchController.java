5f82f50f748075ad51f29f07ea53492dcebe1835
dmdjdj <dtdad@naver.com>
2021-04-01 10:13:31 +0900

    css수정_0401[함혜림]

--- 0gan/src/main/java/com/gan/user/controller/SearchController.java
+++ 0gan/src/main/java/com/gan/user/controller/SearchController.java
@@ -1,12 +1,22 @@
 package com.gan.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gan.user.dao.SearchDao;
+
 @Controller
 public class SearchController {
+	@Autowired
+	private SearchDao dao;
+	
+	public void setDao(SearchDao dao) {
+		this.dao = dao;
+	}
 
+	
 	/**
 	 * localhost/search.do
 	 * -> user/search/search.jsp
@@ -17,6 +27,7 @@ public class SearchController {
 	@RequestMapping("/search.do")
 	public ModelAndView search() {
 		ModelAndView mav = new ModelAndView("/user/search/search");
+		mav.addObject("list",dao.findAll());
 		return mav;
 	}
 
